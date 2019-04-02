aca5d663dbaefade26b61384dfa03324ba27e363
GitGut-Bach <48914962+GitGut-Bach@users.noreply.github.com>
2019-04-02 18:14:46 +0200

    Commit

--- Assets/Scripts/BoardScripts/Bitboard.cs
+++ Assets/Scripts/BoardScripts/Bitboard.cs
@@ -5,24 +5,28 @@ using UnityEngine.UI;
 
 public class Bitboard : MonoBehaviour
 {
+    public BoardRules boardRules = new BoardRules();
 
     public int playerturn = 1;
-
     public int[,] bitboard = new int[8, 8];
-    
+
+    public GameObject BitboardDisplay;
+    public GameObject Blackcountertext;
+    public GameObject Whitecountertext;
+
     // Start is called before the first frame update
     void Start() 
     {
-        GameObject.Find("BitboardDisplay").GetComponent<Text>().text = "     AB CD EF GH";
+        BitboardDisplay.GetComponent<Text>().text = "     AB CD EF GH";
         for (int i = 0; i < 8; i++)
         {
-            GameObject.Find("BitboardDisplay").GetComponent<Text>().text += "\n " + (i + 1) + " [";
+            BitboardDisplay.GetComponent<Text>().text += "\n " + (i + 1) + " [";
             for (int j = 0; j < 8; j++)
             {
                 bitboard[i, j] = 0;
-                GameObject.Find("BitboardDisplay").GetComponent<Text>().text += bitboard[i, j] + " ";
+                BitboardDisplay.GetComponent<Text>().text += bitboard[i, j] + " ";
             }
-            GameObject.Find("BitboardDisplay").GetComponent<Text>().text += "]";
+            BitboardDisplay.GetComponent<Text>().text += "]";
         }
         bitboard[3, 4] = 1; bitboard[4, 3] = 1; bitboard[3, 3] = 2; bitboard[4, 4] = 2;
         bitboardDisplayUpdate();
@@ -38,35 +42,7 @@ public class Bitboard : MonoBehaviour
     public void bitboardUpdate(string Tile)
     {
         var Char = Tile[0];
-        int bitboardX = 0;
-        switch (Char)
-        {
-            case 'A':
-                bitboardX = 0;
-                break;
-            case 'B':
-                bitboardX = 1;
-                break;
-            case 'C':
-                bitboardX = 2;
-                break;
-            case 'D':
-                bitboardX = 3;
-                break;
-            case 'E':
-                bitboardX = 4;
-                break;
-            case 'F':
-                bitboardX = 5;
-                break;
-            case 'G':
-                bitboardX = 6;
-                break;
-            case 'H':
-                bitboardX = 7;
-                break;
-
-        }
+        int bitboardX = char.ToUpper(Char) - 65;
         int bitboardY = Tile[1] - '0';
         bitboard[bitboardY - 1, bitboardX] = playerturn;
         bitboardDisplayUpdate();
@@ -75,15 +51,15 @@ public class Bitboard : MonoBehaviour
 
     void bitboardDisplayUpdate()
     {
-        GameObject.Find("BitboardDisplay").GetComponent<Text>().text = "     AB CD EF GH";
+        BitboardDisplay.GetComponent<Text>().text = "     AB CD EF GH";
         for (int i = 0; i < 8; i++)
         {
-            GameObject.Find("BitboardDisplay").GetComponent<Text>().text +="\n " + (i+1) + " [";
+            BitboardDisplay.GetComponent<Text>().text +="\n " + (i+1) + " [";
             for (int j = 0; j < 8; j++)
             {
-                GameObject.Find("BitboardDisplay").GetComponent<Text>().text += bitboard[i, j] + " ";
+                BitboardDisplay.GetComponent<Text>().text += bitboard[i, j] + " ";
             }
-            GameObject.Find("BitboardDisplay").GetComponent<Text>().text += "]";
+            BitboardDisplay.GetComponent<Text>().text += "]";
         }
     }
 
@@ -109,7 +85,8 @@ public class Bitboard : MonoBehaviour
 
     void pieceCounterUpdate(int Blackpieces, int Whitepieces)
     {
-        GameObject.Find("Blackcountertext").GetComponent<Text>().text = $"{Blackpieces}";
-        GameObject.Find("Whitecountertext").GetComponent<Text>().text = $"{Whitepieces}";
+        boardRules.ValidMove();
+        Blackcountertext.GetComponent<Text>().text = $"{Blackpieces}";
+        Whitecountertext.GetComponent<Text>().text = $"{Whitepieces}";
     }
 }
--- Assets/Scripts/BoardScripts/BoardRules.cs
+++ Assets/Scripts/BoardScripts/BoardRules.cs
@@ -2,21 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardRules : MonoBehaviour
+public class BoardRules
 {
-    public Vector2 up = new Vector2(0, 1);
-    public Vector2 right = new Vector2(1, 0);
-    public Vector2 left = new Vector2(-1, 0);
-    public Vector2 down = new Vector2(0, -1);
-    public Vector2 upright = new Vector2(1, 1);
-    public Vector2 downleft = new Vector2(-1, -1);
-    public Vector2 upleft = new Vector2(-1, 1);
-    public Vector2 downright = new Vector2(1, -1);
+    //public Bitboard boardState = new Bitboard();
+    public int[,] bitboard = new int[8, 8];
+    public int playerturn = 1;
+
+    enum Player { blank = 0, black = 1, white = 2 };
+
+    Vector2[] vectors = new Vector2[]
+        {
+        new Vector2(0, 1),
+        new Vector2(1, 0),
+        new Vector2(-1, 0),
+        new Vector2(0, -1),
+        new Vector2(1, 1),
+        new Vector2(-1, -1),
+        new Vector2(-1, 1),
+        new Vector2(1, -1)
+        };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ValidMove();
     }
 
     // Update is called once per frame
@@ -25,85 +34,57 @@ public class BoardRules : MonoBehaviour
 
     }
 
-    void ValidMove()
+    public void ValidMove()
     {
-        int[,] Tempbitboard = GameObject.Find("The-Board").GetComponent<Bitboard>().bitboard;
-        int playerturn = GameObject.Find("The-Board").GetComponent<Bitboard>().playerturn;
+        int[,] Tempbitboard = bitboard;
 
-        if (playerturn == 1)
+        if (playerturn == (int)Player.black)
         {
             for (int i = 0; i < 8; i++)
             {
                 for (int j = 0; j < 8; j++)
                 {
-
-                }
-            }
-        }
-        else if (playerturn == 2)
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    if (Tempbitboard[i,j] == 2 && CheckForAdjacent(Tempbitboard, i, j, playerturn))
+                    if (Tempbitboard[i, j] == (int)Player.white && CheckForAdjacent(Tempbitboard, i, j, playerturn))
                     {
 
                     }
                 }
             }
         }
-        
 
+        else if (playerturn == (int)Player.white)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (Tempbitboard[i, j] == (int)Player.black && CheckForAdjacent(Tempbitboard, i, j, playerturn))
+                    {
+
+                    }
+                }
+            }
+        }
     }
 
-    public void captureRule(int Tilex, int Tiley)
-    {
-
-    }
-    
     bool CheckForAdjacent(int[,] Tempbitboard, int i, int j, int playerturn)
     {
-        int upHDir = (int)up.x; int upVDir = (int)up.y;
-        int downHDir = (int)down.x; int downVDir = (int)down.y;
-        int leftHDir = (int)left.x; int leftVDir = (int)left.y;
-        int rightHDir = (int)right.x; int rightVDir = (int)right.y;
-        int uprightHDir = (int)upright.x; int uprightVDir = (int)upright.y;
-        int upleftHDir = (int)upleft.x; int upleftVDir = (int)upleft.y;
-        int downrightHDir = (int)downright.x; int downrightVDir = (int)downright.y;
-        int downleftHDir = (int)downleft.x; int downleftVDir = (int)downleft.y;
-
-        
-        int result1 = Tempbitboard[upHDir + i, upVDir + j];
-        int result2 = Tempbitboard[downHDir + i, downVDir + j];
-        int result3 = Tempbitboard[leftHDir + i, leftVDir + j];
-        int result4 = Tempbitboard[rightHDir + i, rightVDir + j];
-        int result5 = Tempbitboard[uprightHDir + i, uprightVDir + j];
-        int result6 = Tempbitboard[upleftHDir + i, upleftVDir + j];
-        int result7 = Tempbitboard[downrightHDir + i, downrightHDir + j];
-        int result8 = Tempbitboard[downleftHDir + i, downleftHDir + j];
-
-        if (playerturn == 2)
+        if (playerturn == (int)Player.white)
         {
-            if (result1 == 1 || result2 == 1 || result3 == 1 || result4 == 1 || result5 == 1 || result6 == 1 || result7 == 1 || result8 == 1)
+            for (int k = 0; k < 8; k++)
             {
-                return true;
+                int getValueX = (int)vectors[k].x;
+                int getValueY = (int)vectors[k].y;
+                Vector2 vector = vectors[k];
+                if (Tempbitboard[i + getValueX, j + getValueY] == (int)Player.black)
+                {
+                    //smid over i liste
+                    Debug.Log("TRUE");
+                }
+                else
+                    Debug.Log("FALSE");
             }
         }
-        else if (playerturn == 1)
-        {
-            if (result1 == 2 || result2 == 2 || result3 == 2 || result4 == 2 || result5 == 2 || result6 == 2 || result7 == 2 || result8 == 2)
-            {
-                return true;
-            }
-        }
-
-
-
-
-            if (result1 != 0)
-            return true;
-        else
-            return false;
+        return true;
     }
 }
