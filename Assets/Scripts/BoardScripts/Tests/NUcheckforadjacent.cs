32a38a25868f59c254bdb6dfcb4b8437bb8b70b1
DanielMorratz <43953231+DanielMorratz@users.noreply.github.com>
2019-05-14 11:51:42 +0200

    Filer med tests af Checkfornine og checkforadjacent

---
+++ Assets/Scripts/BoardScripts/Tests/NUcheckforadjacent.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+class NUnittestCheckForAdjacent
+{
+    BoardRules BR = new BoardRules();
+    public int[,] testboard1 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 2
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 3
+        {0, 0, 0, 2, 1, 0, 0, 0}, // 4
+        {0, 0, 0, 1, 2, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 6
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+    public int[,] expectedboard1 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 2
+        {0, 0, 0, 9, 0, 0, 0, 0}, // 3
+        {0, 0, 9, 2, 1, 0, 0, 0}, // 4
+        {0, 0, 0, 1, 2, 9, 0, 0}, // 5
+        {0, 0, 0, 0, 9, 0, 0, 0}, // 6
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+
+    [Test]
+    public void NUnitAdjacentTest1()
+    {
+        //Arrange
+        testboard1 = BR.ValidMove(testboard1, 1);
+
+        Assert.AreEqual(expectedboard1, testboard1);
+        // Use the Assert class to test conditions
+    }
+    [Test]
+    public void NUnitAdjacentTest1a()
+    {
+        //Arrange
+        testboard1 = BR.ValidMove(testboard1, 2);
+
+        Assert.AreNotEqual(expectedboard1, testboard2);
+        // Use the Assert class to test conditions
+    }
+
+    public int[,] testboard2 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 2
+        {0, 0, 0, 1, 0, 0, 0, 0}, // 3
+        {0, 0, 0, 1, 1, 0, 0, 0}, // 4
+        {0, 0, 0, 1, 2, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 6
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+    public int[,] expectedboard2 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 2
+        {0, 0, 9, 1, 9, 0, 0, 0}, // 3
+        {0, 0, 0, 1, 1, 0, 0, 0}, // 4
+        {0, 0, 9, 1, 2, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 6
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+    [Test]
+    public void NUnitAdjacentTest2()
+    {
+        //Arrange
+        testboard2 = BR.ValidMove(testboard2, 2);
+
+        Assert.AreEqual(expectedboard2, testboard2);
+        // Use the Assert class to test conditions
+    }
+    [Test]
+    public void NUnitAdjacentTest2a()
+    {
+        //Arrange
+        testboard2 = BR.ValidMove(testboard2, 2);
+
+        Assert.AreNotEqual(expectedboard1, testboard2);
+        // Use the Assert class to test conditions
+    }
+
+    public int[,] testboard3 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 1, 0, 0}, // 2
+        {0, 0, 0, 1, 1, 0, 0, 0}, // 3
+        {0, 0, 0, 1, 1, 0, 0, 0}, // 4
+        {0, 0, 0, 1, 1, 2, 0, 0}, // 5
+        {0, 0, 0, 0, 1, 0, 2, 0}, // 6
+        {0, 0, 0, 0, 1, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+    public int[,] expectedboard3 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 9, 0, 0, 1, 0, 0}, // 2
+        {0, 0, 0, 1, 1, 0, 0, 0}, // 3
+        {0, 0, 0, 1, 1, 0, 0, 0}, // 4
+        {0, 0, 9, 1, 1, 2, 0, 0}, // 5
+        {0, 0, 0, 0, 1, 0, 2, 0}, // 6
+        {0, 0, 0, 9, 1, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+    [Test]
+    public void NUnitAdjacentTest3()
+    {
+        //Arrange
+        testboard3 = BR.ValidMove(testboard3, 2);
+
+        Assert.AreEqual(expectedboard3, testboard3);
+        // Use the Assert class to test conditions
+    }
+    [Test]
+    public void NUnitAdjacentTest3a()
+    {
+        //Arrange
+        testboard3 = BR.ValidMove(testboard3, 2);
+
+        Assert.AreNotEqual(null, testboard3);
+        // Use the Assert class to test conditions
+    }
+}
+
+
---
+++ Assets/Scripts/BoardScripts/Tests/NUnitCheckfornine.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class NUnitCheckfornine
+{
+    BoardRules BR = new BoardRules();
+
+    public int[,] testboard1 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 2
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 3
+        {0, 0, 0, 2, 1, 0, 0, 0}, // 4
+        {0, 0, 0, 1, 2, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 6
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+
+    [Test]
+    public void NUnitCheckforNineTest1()
+    {
+        bool exceptedvalue = true;
+        bool actualvalue;
+        actualvalue = BR.CheckForNine(testboard1);
+
+        Assert.AreEqual(exceptedvalue, actualvalue);
+    }
+    [Test]
+    public void NUnitCheckforNineTest2()
+    {
+        bool exceptedvalue = false;
+        bool actualvalue;
+        actualvalue = BR.CheckForNine(testboard1);
+
+        Assert.AreNotEqual(exceptedvalue, actualvalue);
+    }
+
+    public int[,] testboard2 = {
+      // A  B  C  D  E  F  G  H
+        {0, 0, 0, 0, 0, 0, 0, 9}, // 1
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 2
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 3
+        {0, 0, 0, 2, 1, 0, 0, 0}, // 4
+        {0, 0, 0, 1, 2, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 6
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+
+    [Test]
+    public void NUnitCheckforNineTest3()
+    {
+        bool exceptedvalue = false;
+        bool actualvalue;
+        actualvalue = BR.CheckForNine(testboard2);
+
+        Assert.AreEqual(exceptedvalue, actualvalue);
+    }
+    [Test]
+    public void NUnitCheckforNineTest4()
+    {
+        bool exceptedvalue = true;
+        bool actualvalue;
+        actualvalue = BR.CheckForNine(testboard2);
+
+        Assert.AreNotEqual(exceptedvalue, actualvalue);
+    }
+    public int[,] testboard3 = {
+      // A  B  C  D  E  F  G  H
+        {9, 0, 0, 0, 0, 0, 0, 0}, // 1
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 2
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 3
+        {0, 0, 0, 2, 1, 0, 0, 0}, // 4
+        {0, 0, 0, 1, 2, 0, 0, 0}, // 5
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 6
+        {0, 0, 0, 0, 0, 0, 0, 0}, // 7
+        {0, 0, 0, 0, 0, 0, 0, 0}  // 8
+    };
+    [Test]
+    public void NUnitCheckforNineTest5()
+    {
+        bool exceptedvalue = false;
+        bool actualvalue;
+        actualvalue = BR.CheckForNine(testboard3);
+
+        Assert.AreEqual(exceptedvalue, actualvalue);
+    }
+
+}
